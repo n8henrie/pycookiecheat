1092b7a3e6f3b06564becc132af1a2e9a9903c51
Nathan Henrie <n8henrie@gmail.com>
2015-02-26 07:20:39 -0700

    Updated tag number.

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='pycookiecheat',
-    version='0.1.1',
+    version='0.1.2',
     description="Borrow cookies from your browser's authenticated session for use in Python scripts.",
     long_description=readme + '\n\n' + history,
     author='Nathan Henrie',
