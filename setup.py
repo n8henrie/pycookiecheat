c9f1fb6ee3204aea93ac1bfb1d210a8aa62b1a10
Nathan Henrie <nate@n8henrie.com>
2016-05-31 20:53:44 -0600

    Move to src/ subdir
    Also changed to detecting the version from __init__.py instead of
    _version.py

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+import re
+from setuptools import setup, find_packages
 
 try:
     import pypandoc
@@ -16,44 +13,46 @@ except ImportError:
     with open('HISTORY.md') as history_file:
         history = history_file.read()
 
-exec(open('pycookiecheat/_version.py').read())
+version_regex = re.compile(r'__version__ = [\'\"]((\d+\.?)+)[\'\"]')
+with open('src/pycookiecheat/__init__.py') as f:
+    vlines = f.readlines()
+__version__ = next(re.match(version_regex, line).group(1) for line in vlines
+                   if re.match(version_regex, line))
 
 with open('requirements.txt') as requirements_file:
     requirements = requirements_file.read().splitlines()
 
-
-test_requirements = [
-    'pytest>=2.6.4'
-]
+with open('requirements-dev.txt') as dev_requirements_file:
+    dev_requirements = dev_requirements_file.read().splitlines()
 
 setup(
     name='pycookiecheat',
-    version=__version__,  # noqa
+    version=__version__,
     description="Borrow cookies from your browser's authenticated session for"
                 "use in Python scripts.",
     long_description=readme + '\n\n' + history,
     author='Nathan Henrie',
     author_email='nate@n8henrie.com',
     url='https://github.com/n8henrie/pycookiecheat',
-    packages=[
-        'pycookiecheat',
-    ],
-    package_dir={'pycookiecheat':
-                 'pycookiecheat'},
+    packages=find_packages('src'),
+    package_dir={"": "src"},
     include_package_data=True,
     install_requires=requirements,
-    license="GPLv3",
+    license="MIT",
     zip_safe=False,
-    keywords='pycookiecheat',
+    keywords='pycookiecheat chrome cookies',
     classifiers=[
         'Natural Language :: English',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
+    extras_require={
+        "dev": dev_requirements
+        },
     test_suite='tests',
-    tests_require=test_requirements
+    tests_require=['pytest==2.9.2']
 )
