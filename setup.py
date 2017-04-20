2be7003df88f397f4fe77ac87293c74f38e59613
Nathan Henrie <nate@n8henrie.com>
2017-04-20 08:58:16 -0600

    Remove Python2 and Python3.4 compatibility

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 import re
 from setuptools import setup, find_packages
 
@@ -41,12 +40,9 @@ setup(
     keywords='pycookiecheat chrome cookies',
     classifiers=[
         'Natural Language :: English',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     extras_require={
         "dev": dev_requirements
