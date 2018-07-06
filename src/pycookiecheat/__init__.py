a7b4e4dbe93b39354f39e3f214532c8ebcdf4b81
Nathan Henrie <nate@n8henrie.com>
2018-07-06 09:55:59 -0600

    Linter and mypy fixes

--- src/pycookiecheat/__init__.py
+++ src/pycookiecheat/__init__.py
@@ -1,7 +1,11 @@
 """__init__.py :: Exposes chrome_cookies function."""
 
-from pycookiecheat.pycookiecheat import chrome_cookies  # noqa
+from pycookiecheat.pycookiecheat import chrome_cookies
 
 __author__ = 'Nathan Henrie'
 __email__ = 'nate@n8henrie.com'
 __version__ = '0.4.3'
+
+__all__ = [
+        "chrome_cookies",
+        ]
