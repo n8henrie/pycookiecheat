e1c780358b112b84192a45eac078205b880f8b9d
jakob <grandchild@gmx.net>
2023-07-02 10:08:01 +0200

    Add common Cookie dataclass
    Reduces the code duplication around the curl-cookie-file a bit.

--- src/pycookiecheat/common.py
+++ src/pycookiecheat/common.py
@@ -1,5 +1,39 @@
 """Common code for pycookiecheat."""
+from dataclasses import dataclass
 from typing import Iterator
+
+
+@dataclass
+class Cookie:
+    """Internal helper class used to represent a cookie only during processing.
+
+    Cookies returned to the user from the public API are dicts, not instances
+    of this class.
+    """
+
+    name: str
+    value: str
+    host_key: str
+    path: str
+    expires_utc: int
+    is_secure: int
+
+    def as_cookie_file_line(self) -> str:
+        """Return a string for a Netscape-style cookie file usable by curl.
+
+        See details athttp://www.cookiecentral.com/faq/#3.5
+        """
+        return "\t".join(
+            [
+                self.host_key,
+                "TRUE",
+                self.path,
+                "TRUE" if self.is_secure else "FALSE",
+                str(self.expires_utc),
+                self.name,
+                self.value,
+            ]
+        )
 
 
 def generate_host_keys(hostname: str) -> Iterator[str]:
