945dcd8ba01ec72b7c847294dce1b1978b87209c
Nathan Henrie <nate@n8henrie.com>
2024-05-13 22:07:22 -0600

    format

--- flake.nix
+++ flake.nix
@@ -3,74 +3,64 @@
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    inherit (nixpkgs) lib;
-    systems = ["aarch64-darwin" "x86_64-linux" "aarch64-linux"];
-    systemClosure = attrs:
-      builtins.foldl'
-      (acc: system:
-        lib.recursiveUpdate acc (attrs system))
-      {}
-      systems;
-  in
+  outputs =
+    { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      systems = [
+        "aarch64-darwin"
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+      systemClosure =
+        attrs: builtins.foldl' (acc: system: lib.recursiveUpdate acc (attrs system)) { } systems;
+    in
     systemClosure (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
         pname = "pycookiecheat";
         propagatedBuildInputs = with pkgs.python311Packages; [
           cryptography
           keyring
         ];
-        pycookiecheat = {
-          lib,
-          python311,
-        }:
+        pycookiecheat =
+          { lib, python311 }:
           python311.pkgs.buildPythonPackage {
             inherit pname;
-            version =
-              builtins.elemAt
-              (lib.splitString "\""
-                (lib.findSingle
-                  (val: builtins.match "^__version__ = \".*\"$" val != null)
-                  (abort "none")
-                  (abort "multiple")
-                  (lib.splitString "\n"
-                    (builtins.readFile ./src/pycookiecheat/__init__.py))))
-              1;
+            version = builtins.elemAt (lib.splitString "\"" (
+              lib.findSingle (val: builtins.match "^__version__ = \".*\"$" val != null) (abort "none")
+                (abort "multiple")
+                (lib.splitString "\n" (builtins.readFile ./src/pycookiecheat/__init__.py))
+            )) 1;
 
             src = lib.cleanSource ./.;
             pyproject = true;
-            nativeBuildInputs = with pkgs.python311Packages; [
-              setuptools-scm
-            ];
+            nativeBuildInputs = with pkgs.python311Packages; [ setuptools-scm ];
             inherit propagatedBuildInputs;
           };
-      in {
+      in
+      {
         packages.${system} = {
-          ${pname} = pkgs.callPackage pycookiecheat {};
-          default = pkgs.python311.withPackages (_: [self.packages.${system}.${pname}]);
+          ${pname} = pkgs.callPackage pycookiecheat { };
+          default = pkgs.python311.withPackages (_: [ self.packages.${system}.${pname} ]);
         };
 
         devShells.${system}.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (python38.withPackages (ps: [ps.cffi]))
-            (python39.withPackages (ps: [ps.cffi]))
-            (python310.withPackages (ps: [ps.cffi]))
+            (python38.withPackages (ps: [ ps.cffi ]))
+            (python39.withPackages (ps: [ ps.cffi ]))
+            (python310.withPackages (ps: [ ps.cffi ]))
             # https://github.com/NixOS/nixpkgs/issues/277591
-            (python312.withPackages (ps: [ps.cffi]))
+            (python312.withPackages (ps: [ ps.cffi ]))
             (python311.withPackages (
               ps:
-                propagatedBuildInputs
-                ++ (with ps; [
-                  mypy
-                  pytest
-                  tox
-                ])
+              propagatedBuildInputs
+              ++ (with ps; [
+                mypy
+                pytest
+                tox
+              ])
             ))
           ];
         };
